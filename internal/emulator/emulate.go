c85587f553354758296a9dd99233a731e5624711
braheezy <michael.c.braha@gmail.com>
2024-03-29 10:56:32 -0700

    Many instructions + start cycle counting

--- internal/emulator/emulate.go
+++ internal/emulator/emulate.go
@@ -1,21 +1,46 @@
 package emulator
 
 func (vm *CPU8080) nextOpCode() {
-	currentCode := vm.memory[vm.pc : vm.pc+3]
 
-	op := currentCode[0]
-	vm.pc += 1
+	for {
+		currentCode := vm.memory[vm.pc : vm.pc+3]
 
-	if opcodeFunc, exists := vm.opcodeTable[op]; exists {
-		opcodeFunc(currentCode[1:])
-	} else {
-		vm.Logger.Fatalf("Unsupported opcode: %02X", op)
+		op := currentCode[0]
+		vm.pc++
+		vm.cycleCount++
+
+		if opcodeFunc, exists := vm.opcodeTable[op]; exists {
+			opcodeFunc(currentCode[1:])
+		} else {
+			vm.Logger.Fatalf("Unsupported opcode: %02X", op)
+		}
 	}
 
 }
 
 func toUint16(code *[]byte) uint16 {
 	return uint16((*code)[1])<<8 | uint16((*code)[0])
+}
+
+func auxCarrySub(a, b byte) bool {
+	// Check if borrow is needed from higher nibble to lower nibble
+	return (a & 0xF) < (b & 0xF)
+}
+
+// func auxCarryAdd(a, b byte) bool {
+// 	// Check if carry is needed from higher nibble to lower nibble
+// 	return (a & 0xF) > (b & 0xF)
+// }
+
+func parity(x uint16) bool {
+	y := x ^ (x >> 1)
+	y = y ^ (y >> 2)
+	y = y ^ (y >> 4)
+	y = y ^ (y >> 8)
+
+	// Rightmost bit of y holds the parity value
+	// if (y&1) is 1 then parity is odd else even
+	return y&1 > 0
 }
 
 // NOP: No operation.
@@ -26,69 +51,129 @@ func (vm *CPU8080) nop(data []byte) {
 // JMP: Jump to address.
 func (vm *CPU8080) jump(data []byte) {
 	operand := toUint16(&data)
-	vm.Logger.Debugf("[C3] JMP to $%04X", operand)
+	vm.Logger.Debugf("[C3] JMP \t$%04X", operand)
 	vm.pc = operand
+	vm.cycleCount += 2
 }
 
 // LXI SP, D16: Load 16-bit immediate value into register pair SP.
 func (vm *CPU8080) loadSP(data []byte) {
 	operand := toUint16(&data)
-	vm.Logger.Debugf("[31] LXI SP, $%04X", operand)
+	vm.Logger.Debugf("[31] LD  \tSP,$%04X", operand)
 	vm.sp = operand
 	vm.pc += 2
+	vm.cycleCount += 2
 }
 
 // LXI B, D16: Load 16-bit immediate value into register pair B.
 func (vm *CPU8080) loadBC(data []byte) {
-	vm.Logger.Debugf("[01] LXI B, $%04X", toUint16(&data))
+	vm.Logger.Debugf("[01] LD  \tB,$%04X", toUint16(&data))
 	vm.registers.C = data[0]
 	vm.registers.B = data[1]
 	vm.pc += 2
+	vm.cycleCount += 2
 }
 
 // MVI B, D8: Move 8-bit immediate value into register B.
 func (vm *CPU8080) moveB(data []byte) {
-	vm.Logger.Debugf("[06] MVI B, $%02X", data[0])
+	vm.Logger.Debugf("[06] LD  \tB,$%02X", data[0])
 	vm.registers.B = data[0]
-	vm.pc += 1
+	vm.pc++
+	vm.cycleCount++
 }
 
 // CALL addr: Call subroutine at address
 func (vm *CPU8080) call(data []byte) {
 	operand := toUint16(&data)
-	vm.Logger.Debugf("[CD] CALL $%04X", operand)
+	vm.Logger.Debugf("[CD] CALL\t$%04X", operand)
 	vm.pc = operand
 	vm.memory[vm.sp-1] = data[1]
 	vm.memory[vm.sp-2] = data[0]
 	vm.sp -= 2
+	vm.cycleCount += 5
 }
 
 // LXI D, D16: Load 16-bit immediate value into register pair D.
 func (vm *CPU8080) loadDE(data []byte) {
-	vm.Logger.Debugf("[11] LXI D, $%04X", toUint16(&data))
+	vm.Logger.Debugf("[11] LD  \tDE,$%04X", toUint16(&data))
 	vm.registers.E = data[0]
 	vm.registers.D = data[1]
 	vm.pc += 2
+	vm.cycleCount += 2
 }
 
 // LXI H, D16: Load 16-bit immediate value into register pair H.
 func (vm *CPU8080) loadHL(data []byte) {
-	vm.Logger.Debugf("[21] LXI H, $%04X", toUint16(&data))
+	vm.Logger.Debugf("[21] LD  \tHL,$%04X", toUint16(&data))
 	vm.registers.L = data[0]
 	vm.registers.H = data[1]
 	vm.pc += 2
+	vm.cycleCount += 2
 }
 
 // LDAX D: Load value from address in register pair D into accumulator.
 func (vm *CPU8080) loadAXD(data []byte) {
 	address := toUint16(&[]byte{vm.registers.D, vm.registers.E})
-	vm.Logger.Debugf("[1A] LDAX D")
+	vm.Logger.Debugf("[1A] LD  \tA,(DE)")
 	vm.registers.A = vm.memory[address]
+	vm.cycleCount += 2
 }
 
 // MOV M, A: Move value from accumulator into register pair H.
-func (vm *CPU8080) moveMA(data []byte) {
+func (vm *CPU8080) storeHLA(data []byte) {
 	address := toUint16(&[]byte{vm.registers.H, vm.registers.L})
-	vm.Logger.Debugf("[22] MOV M, A")
+	vm.Logger.Debugf("[22] LD  \t(HL),A")
 	vm.memory[address] = vm.registers.A
+	vm.cycleCount += 5
+}
+
+// INC H: Increment register pair H.
+func (vm *CPU8080) incHL(data []byte) {
+	vm.Logger.Debugf("[23] INC \tHL")
+	vm.registers.H++
+	vm.registers.L++
+	vm.cycleCount++
+}
+
+// INC D: Increment register pair D.
+func (vm *CPU8080) incDE(data []byte) {
+	vm.Logger.Debugf("[13] INC \tDE")
+	vm.registers.D++
+	vm.registers.E++
+	vm.cycleCount++
+}
+
+// DCR B: Decrement register B.
+func (vm *CPU8080) decB(data []byte) {
+	vm.Logger.Debugf("[05] DEC \tB")
+	result := uint16(vm.registers.B) - 1
+
+	// Handle condition bits
+	vm.flags.setZ(result)
+	vm.flags.setS(result)
+	vm.flags.H = auxCarrySub(vm.registers.B, 1)
+	vm.flags.setP(result)
+
+	vm.registers.B--
+	vm.cycleCount++
+}
+
+// JNZ addr: Jump if not zero.
+func (vm *CPU8080) jumpNZ(data []byte) {
+	operand := toUint16(&data)
+	vm.Logger.Debugf("[20] JP  \tNZ,$%04X", operand)
+	if !vm.flags.Z {
+		vm.pc = operand
+	} else {
+		vm.pc += 2
+	}
+	vm.cycleCount++
+}
+
+// RET: Return from subroutine.
+func (vm *CPU8080) ret(data []byte) {
+	vm.Logger.Debugf("[C9] RET")
+	vm.pc = toUint16(&[]byte{vm.memory[vm.sp+1], vm.memory[vm.sp]})
+	vm.sp += 2
+	panic("stop")
 }
--- internal/emulator/main.go
+++ internal/emulator/main.go
@@ -19,13 +19,15 @@ type CPU8080 struct {
 	// sp is the stack pointer, the index to memory
 	sp uint16
 	// flags indicate the results of arithmetic and logical operations
-	// flags flags
+	flags flags
 	// Logger object to use
 	Logger *log.Logger
 	// Lookup table of opcode functions
 	opcodeTable map[byte]opcodeExec
 	// Options are the current options to use on the emulator
 	Options CPU8080Options
+	// For timing sync
+	cycleCount int
 }
 
 type CPU8080Options struct {
@@ -36,13 +38,23 @@ type registers struct {
 	A, B, C, D, E, H, L byte
 }
 
-// type flags struct {
-// 	Z bool
-// 	S bool
-// 	H bool
-// 	C bool
-// 	P bool
-// }
+type flags struct {
+	Z bool
+	S bool
+	H bool
+	C bool
+	P bool
+}
+
+func (fl *flags) setZ(value uint16) {
+	fl.Z = value == 0
+}
+func (fl *flags) setS(value uint16) {
+	fl.S = value&0x80 != 0
+}
+func (fl *flags) setP(value uint16) {
+	fl.P = parity(value)
+}
 
 type opcodeExec func([]byte)
 
@@ -50,19 +62,26 @@ func NewCPU8080(program *[]byte) *CPU8080 {
 	vm := &CPU8080{
 		Logger: log.New(os.Stdout),
 	}
-	println(vm.Options.ProgramStartAddress)
+	// Put the program into memory at the location it wants to be
 	copy(vm.memory[vm.Options.ProgramStartAddress:], *program)
 	vm.programSize = len(*program) + int(vm.Options.ProgramStartAddress)
+
+	// Define all supported opcodes
 	vm.opcodeTable = map[byte]opcodeExec{
 		0x00: vm.nop,
 		0x01: vm.loadBC,
+		0x05: vm.decB,
 		0x06: vm.moveB,
 		0x11: vm.loadDE,
+		0x13: vm.incDE,
 		0x1A: vm.loadAXD,
 		0x21: vm.loadHL,
+		0x23: vm.incHL,
 		0x31: vm.loadSP,
-		0x77: vm.moveMA,
+		0x77: vm.storeHLA,
+		0xC2: vm.jumpNZ,
 		0xC3: vm.jump,
+		// 0xC9: vm.ret,
 		0xCD: vm.call,
 	}
 
